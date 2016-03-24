3929fcdca43e80d605cef365b902a6a6f62437f5
Tianyang Hu <tianyang.hu01@gmail.com>
2016-03-23 22:10:22 -0700

    Updated route.go to support template

--- route/route.go
+++ route/route.go
@@ -1,19 +1,30 @@
 package route
 
 import (
-    "io"
+    "html/template"
     "net/http"
+    //"path"
+    "os"
+    "path/filepath"
+    "fmt"
     "github.com/gorilla/mux"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+func Index(w http.ResponseWriter, r *http.Request) {
+    cwd, _ := os.Getwd()
+    fmt.Println( filepath.Join( cwd, "template/index.html" ) )
+    templates := template.Must(template.ParseFiles("templates/index.html"))
+    err := templates.ExecuteTemplate(w, "index", nil)
+     if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
 }
 
 func Routes() *mux.Router{
     r := mux.NewRouter()
     // Routes consist of a path and a handler function.
-    r.HandleFunc("/", hello)
+    r.HandleFunc("/", Index)
     
     return r
 }
