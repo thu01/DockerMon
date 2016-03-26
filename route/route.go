5b727dbef9c05962233476a94ab8b5e93ae5d2af
Tianyang Hu <tianyang.hu01@gmail.com>
2016-03-25 23:18:12 -0700

    Updated route to support REST service for registration

--- route/route.go
+++ route/route.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+    "encoding/json"
     "html/template"
     "net/http"
     //"path"
@@ -20,6 +21,11 @@ type UserInf struct {
     Email string
 }
 
+type Response struct {
+    Code int
+    Message interface{}
+}
+
 func Index(w http.ResponseWriter, r *http.Request) {
     cwd, _ := os.Getwd()
     fmt.Println( filepath.Join( cwd, "template/index.html" ) )
@@ -31,23 +37,33 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func RegisterPOST(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("RegisterPOST")
+    
     username := r.FormValue("username")
     email := r.FormValue("email")
     password := r.FormValue("password")
     user := &UserInf { username, password, email}
-    err := templates.ExecuteTemplate(w, "about", user)
-     if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
+    statusCode := http.StatusOK
+    userJson, err := json.Marshal(user)
+    if err != nil {
+        statusCode = http.StatusInternalServerError
     }
+    
+    fmt.Println(string(userJson))
+    response := &Response{statusCode, userJson}
+    responseJson, err := json.Marshal(response)
+    if err != nil {
+        fmt.Println(err)
+    }
+    fmt.Println(string(responseJson))
+    w.Write(responseJson)
 }
 
 func Routes() *mux.Router{
     r := mux.NewRouter()
     // Routes consist of a path and a handler function.
     //r.HandleFunc("/", Index)
+    r.HandleFunc("/api/users", RegisterPOST).Methods("POST")
     r.PathPrefix("/").Handler(http.FileServer(http.Dir("./client")))
-    r.HandleFunc("/register", RegisterPOST).Methods("POST")
-    
     return r
 }
