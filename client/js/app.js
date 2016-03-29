9bff003bc1de9ed1266d48fb192b186f02a27e91
Tianyang Hu <tianyang.hu01@gmail.com>
2016-03-28 22:40:01 -0700

    Added placehoder for front-end files\n, modified app.js to check if username already exists

--- client/js/app.js
+++ client/js/app.js
@@ -55,9 +55,13 @@ app.controller('RegisterController', ['UserService', '$scope', '$location', func
     $scope.user = {}
     $scope.user.username = 'thu';
     $scope.user.email = 'thu@gmail.com';
-    $scope.user.password='123';
-    $scope.user.verifyPassword='123';
+    $scope.user.password='12345';
+    $scope.user.verifyPassword='12345';
     $scope.register = function(user) {
+        console.log($scope.registerForm.$pending);
+        if($scope.registerForm.$pending !== undefined && $scope.registerForm.$pending.username){
+            return;
+        }
         UserService.Register(user).then(function(response){
             if(response.Status==200) {
                 $location.path('/about');
@@ -90,4 +94,33 @@ app.directive('compareTo', function() {
             });
         }
     };
+});
+
+app.directive('username', function($q, $timeout) {
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      var usernames = ['Jim', 'John', 'Jill', 'Jackie'];
+      ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+        if (ctrl.$isEmpty(modelValue)) {
+          // consider empty model valid
+          return $q.when();
+        }
+        var def = $q.defer();
+        $timeout(function() {
+          // Mock a delayed response
+          if (usernames.indexOf(modelValue) === -1) {
+            // The username is available
+            ctrl.$setValidity('validUsername', true);
+            def.resolve();
+          } else {
+            ctrl.$setValidity('validUsername', false);
+            def.reject();
+          }
+
+        }, 2000);
+        return def.promise;
+      };
+    }
+  };
 });
