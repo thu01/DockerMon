e7418cc01c327be0b7bb4f5f8a237e9d610bac1c
Tianyang Hu <tianyang.hu01@gmail.com>
2016-04-07 00:17:34 -0700

    Added handler for login with session support

--- route/route.go
+++ route/route.go
@@ -2,11 +2,11 @@ package route
 
 import (
     "encoding/json"
-    //"html/template"
     "net/http"
     //"path"
     "fmt"
     "github.com/gorilla/mux"
+    "github.com/gorilla/sessions"
 )
 
 const (
@@ -24,6 +24,17 @@ type UserInf struct {
 type Response struct {
     Status int
     Message interface{}
+}
+
+var sessionStore = sessions.NewCookieStore([]byte("GoWebApp-Session-Store"))
+
+func init() {
+    sessionStore.Options = &sessions.Options{
+        Domain:   "localhost",
+        Path:     "/",
+        MaxAge:   3600 * 8, // 8 hours
+        HttpOnly: true,
+    }
 }
 
 func NotFound(w http.ResponseWriter, r *http.Request) {
@@ -74,7 +85,61 @@ func UserGET(w http.ResponseWriter, r *http.Request) {
 }
 
 func SessionPOST(w http.ResponseWriter, r *http.Request) {
+    userMap := map[string]UserInf{
+        "thu": UserInf{USER_STATUS_NORMAL, "thu", "12345", "thu@gmail.com"},
+        "lss": UserInf{USER_STATUS_NORMAL, "lss", "12345", "lss@gmail.com"},
+    }
+
+    statusCode := http.StatusOK
+    session, err := sessionStore.Get(r, "GoWebApp-Login-Session");
+    if err!=nil {
+        fmt.Println(err)
+        response := &Response{http.StatusInternalServerError, http.StatusText(http.StatusInternalServerError)}
+        responseJson, _ := json.Marshal(response)
+        w.Write(responseJson)
+        return
+    }
     
+    username := r.FormValue("username")
+    password := r.FormValue("password")
+    for k,v := range session.Values {
+        fmt.Println(k, "=", v)
+    } 
+    if session.Values["username"]==username {
+        user, _ := userMap[username]
+        response := &Response{statusCode, user}
+        responseJson, err := json.Marshal(response)
+        if err != nil {
+        	    fmt.Println(err)
+        }
+        w.Write(responseJson)
+        return
+    }
+    
+    user, ok := userMap[username]
+    if !ok {
+        statusCode = http.StatusNotFound
+        response := &Response{statusCode, http.StatusText(statusCode)}
+        responseJson, _ := json.Marshal(response)
+        w.Write(responseJson)
+        return
+    }
+    if user.Password != password {
+        statusCode = http.StatusUnauthorized
+        response := &Response{statusCode, http.StatusText(statusCode)}
+        responseJson, _ := json.Marshal(response)
+        w.Write(responseJson)
+        return
+        
+    }
+    response := &Response{statusCode, user}
+    responseJson, err := json.Marshal(response)
+    if err != nil {
+        fmt.Println(err)
+    }
+    session.Values["username"]=username
+    session.Save(r,w)
+    w.Write(responseJson)
 }
 
 func Routes() *mux.Router{
